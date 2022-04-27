14e7f2ba7bda4bf4acc5fd10af27e43ed2f40ab8
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-04-27 11:35:47 -0300

    Add files via upload

---
+++ Lampada/src/lampada.java
@@ -0,0 +1,41 @@
+
+public class lampada {
+
+	
+	
+	private boolean ligado; 
+
+	public lampada() {
+		ligado = true;	
+		System.out.println("ligado");
+	
+	}
+		public void ligar() {
+			ligado = true;
+		if (ligado == true) {
+			System.out.println("Sua lampada está ligada");
+		}
+		}
+		
+		public void desligar() {
+			ligado = false;
+		if (ligado == false) {
+			System.out.println("Sua lampada está desligada");
+		}
+		}
+
+			
+	
+	
+	
+
+	
+
+	
+		
+
+		
+		}
+	
+	
+
---
+++ Lampada/src/main.java
@@ -0,0 +1,15 @@
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		lampada lampada = new lampada();
+		lampada.ligar();
+		System.out.println("------------------------------------------------");
+		System.out.println("desligado");
+		lampada.desligar();	
+
+	}
+	
+
+}
