2d8d5862318e678f4f594bf4464c22175548cbbb
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-05-02 11:37:36 -0300

    Add files via upload

---
+++ ElemetosStaticos/src/aplicacao.java
@@ -0,0 +1,45 @@
+
+public class aplicacao {
+
+	public static void main(String[] args) {
+	
+		Matematica matematica =  new Matematica();
+//		
+//		int resultado = Matematica.somar(2, 3);
+//
+//		System.out.println(resultado );
+//		resultado = Matematica.subtrair(10, 4);
+//		System.out.println(resultado);
+//		
+//		int resultado = Matematica.somar(2, 4);
+//		System.out.println(resultado);
+//		resultado = Matematica.subtrair(10, 4);
+//		System.out.println(resultado);
+		
+		//static- mudar atributos sem mudar classe
+	
+//		Contador contador1 = new Contador();
+//		Contador contador2 = new Contador();
+//		Contador contador3 = new Contador();
+//		contador1.incrementar();
+//		
+//		System.out.println(contador1.getValor());
+//		System.out.println(contador2.getValor());
+//		System.out.println(contador3.getValor());
+//		
+//		Contador.incrementar();
+//		Contador.incrementar();
+//		Contador.incrementar();
+//		System.out.println(Contador.getValor());
+//		
+		System.out.println(Constantes.MEDIA_DA_PROVA);
+		
+		
+		
+		
+		
+				
+	}
+	
+
+}
