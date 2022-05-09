25080d54ae3f28d55ac4befed5ba442909f7e06a
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-05-09 08:58:31 -0300

    Add files via upload

---
+++ Loja/src/Venda.java
@@ -0,0 +1,39 @@
+
+public class Venda {
+
+	public static void main(String[] args) {
+		double prod1 = 500; 
+		double prod2 = 100; 
+		double desc= 0.85;
+		double total = (prod1 + prod2) * desc; 
+		
+		System.out.println ("O pruduto 1 custa " + prod1);
+		System.out.println (" O produto 2 custa " + prod2);
+		
+	
+				("O Valor total com um desconto de 15% é de " +  total );
+				
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+				
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ Loja2/src/LojaTop.java
@@ -0,0 +1,26 @@
+
+public class LojaTop {
+
+	public static void main(String[] args) {
+		double prod1 = 70; 
+		double prod2 = 120;
+		double prod3 = 40; 
+		double prod4 = 65;
+		double desc = 0.58;
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite a operação desejada");
+		System.out.println("Calça Jeans Feminina-120,00");
+		System.out.println("Camiseta Nike Masculina- 70,00");
+		System.out.println("Vestido longo 45,00");
+		System.out.println("Mochila-65,00");
+		
+		
+		
+		
+		
+
+	}
+
+}
+;
