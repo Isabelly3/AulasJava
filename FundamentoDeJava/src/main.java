fb43e834abbe0e354b1d7b0ea44cb1c0fa861f6b
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-05-09 08:56:48 -0300

    Add files via upload

---
+++ FundamentoDeJava/src/main.java
@@ -0,0 +1,130 @@
+import java.util.Scanner;
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	
+	// TODO Auto-generated method stub
+			// int x = 100;
+
+			// double y = 200;
+
+			// int a = 0;
+			// double b = 0;
+
+			// a = (int)y;
+			// b = x;
+
+			// System.out.println(a);
+			// System.out.println(b);
+
+//			//int idadePessoa = 10;
+	//
+//			//if (idadePessoa < 18) {
+//				//System.out.println("Você não pode beber");
+//			//}
+	//
+//			//else if (idadePessoa < 60) { //de uma condição
+//			//	System.out.println("Bora tomar uma");
+//			}else {
+//				System.out.println("Vai dormir idoso!");
+//			}
+	//
+//			System.out.println("Você tem " + idadePessoa + " anos");
+			
+//			
+//			int diasSemana = 8;
+	//
+//			 switch(diasSemana) {
+//			 	case 1 :
+//			 		System.out.println("Domingo");
+//			 		break;
+//			 	case 2:
+//			 		System.out.println("Segunda");
+//			 		break;
+//			 	case 3:
+//			 		System.out.println("Terça");
+//			 		break;
+//			 	case 4:
+//			 		System.out.println("Quarta");
+//			 		break;
+//			 	case 5:
+//			 		System.out.println("Quinta");
+//			 		break;
+//			 	case 6:
+//			 		System.out.println("Sexta");
+//			 		break;
+//			 	case 7:
+//			 		System.out.println("Sábado");
+//			 		break;
+//			 	default:
+//			 		System.out.println("Esse número não representa um dia da semana");
+//			 	
+//		}
+			
+			
+	//
+	//}
+	//}
+			
+			
+//			char caractere = 'a';
+//					
+//					switch(caractere) {
+//					case'a':
+//						System.out.println("Seu caractere é a");
+//						break;
+//					case 'b':
+//						System.out.println("Seu caractere é b");
+//						break;
+//					default:
+//						System.out.println("Valor inválido");
+//						
+//					}
+	//
+//		}
+//		}
+			
+//			String farol = "vermelho";
+//			
+//			switch(farol) {
+//			case "vermelho":
+//				System.out.println("Tampa a placa e vai");
+//				break;
+//			case "amarelo":
+//				System.out.println("Celeraa muito");
+//				break;
+//			case "verde":
+//				System.out.println("Celera mas agora legalmente");
+//				break;
+//				default:
+//					System.out.println("Cê é burro?");
+//			}}}
+			
+			
+//			
+
+			
+//			for(int x = 2; x<11; x++ ) {
+//				System.out.println(x);
+//			}
+//		}
+	//}
+			
+			int valorUsuario;
+			int multiplicador = 0;
+			int total = 0;
+			
+			Scanner ler = new Scanner(System.in);
+			System.out.println("Informe um número: ");
+			valorUsuario = ler.nextInt();;
+			
+			for (int x = 0; x<10; x++) {
+				total = valorUsuario * ++multiplicador;
+				System.out.println(valorUsuario +  "x" + multiplicador + " = " + total);
+	}
+	}
+	}
+
+
