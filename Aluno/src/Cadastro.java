11d67ed7f7e023b676aefbc373016e434c07c445
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-05-09 08:57:31 -0300

    Add files via upload

---
+++ Aluno/src/Cadastro.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Cadastro 
+{
+		Scanner scan= new Scanner(System.in);
+		private String nome,cpf,idade,tel,escola;
+		private Date nasc;
+		
+		void Cadastro(String nome,String cpf,String idade)
+		{
+			this.nome = nome;
+			this.cpf=cpf;
+			this.idade=idade;
+		}
+		
+	
+}
