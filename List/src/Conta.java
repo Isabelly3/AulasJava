f70155e2dadd23e60820093aec1cfa3a787adac0
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-05-19 11:45:01 -0300

    Add files via upload

---
+++ List/src/Conta.java
@@ -0,0 +1,26 @@
+
+public class Conta {
+	
+	
+	String nome;
+	double valor;
+	
+	public Conta(String nome, double valor) {
+	
+		this.nome = nome;
+		this.valor = valor;
+	}
+
+	@Override
+	public String toString() {
+		return "Conta [nome=" + nome + ", valor=" + valor + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+
+}
---
+++ List/src/main.java
@@ -0,0 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class main {
+
+	public static void main(String[] args) {
+		
+		List lista =  new ArrayList();
+		
+//		lista.add("abc");
+//		lista.add(12);
+//		lista.add(true);
+//		for(int x=0; x< lista.size(); x++) {
+//			System.out.println(lista.get(x));
+//		}
+//		for(Object valor : lista) {
+//			System.out.println(valor);
+//		}
+//		
+//		System.out.println();
+//		lista.remove(0);
+//		for(Object valor : lista) {
+//			System.out.println(valor);	
+//		}
+//		
+//		System.out.println();
+//		
+//		lista.add(0, lista);
+//		for(Object valor : lista) {
+//			System.out.println(valor);
+//		
+//		}
+//		System.out.println();
+//		
+//		lista.set(0, "ghi");
+//		for(Object valor : lista) {
+//			System.out.println(valor);
+//		}
+//		System.out.println();
+//		
+//		lista.clear();
+//		if(lista.isEmpty()) {
+//			System.out.println("sua lista está vazia");
+//		}
+
+
+//		Conta conta1 =  new Conta("Água" , 90.00);
+//		Conta conta2 =  new Conta("Luz" , 135.00);
+//		
+//		List<Conta> listaDeContas = new ArrayList<Conta>();
+//		
+//		listaDeContas.add(conta1);
+//		listaDeContas.add(conta2);
+		
+//		for(Conta conta : listaDeContas) {
+//			System.out.println(conta);
+		}
+		
+				
+	}
+
+}
