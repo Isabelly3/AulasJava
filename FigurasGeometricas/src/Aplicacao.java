23895c9a35d90b923613103fd2c5054338d7ef45
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-06-09 03:12:02 -0300

    Update Aplicacao.java

--- FigurasGeometricas/src/Aplicacao.java
+++ FigurasGeometricas/src/Aplicacao.java
@@ -3,17 +3,21 @@ public class Aplicacao {
 
 	public static void main(String[] args) {
 		
+		
+		 //estanciando quadrado e dando o valor de  cada lado
 //				Quadrado quadrado = new Quadrado();
 //				quadrado.setLado(50);
 //				  
-//				quadrado.calcularArea();
+//				quadrado.calcularArea(); //chamando metodo calcular quadrado
 //				
+		//estanciando triangulo e dando o valor de  cada lado
 //				Triangulo triangulo = new Triangulo();
 //				triangulo.setBase(15);
 //				triangulo.setAltura(10); 
 //				
 //				triangulo.calcularArea();
 				
+				//estanciando circulo e dando seu valor
 				
 				Circulo circulo = new Circulo();
 				circulo.setRaio(7);
