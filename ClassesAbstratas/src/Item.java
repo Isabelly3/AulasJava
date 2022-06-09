5486af9336e3c9fe9ebc6d2581490080f7dc6eb0
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-06-09 02:38:51 -0300

    Update Item.java

--- ClassesAbstratas/src/Item.java
+++ ClassesAbstratas/src/Item.java
@@ -4,11 +4,13 @@ public abstract class Item {
 	private int posX;
 	private int posY;
 	
+	
+	//ações necessarias 
 	public abstract void pegar() ;
 		
 		
-		
-	
+	//retorna valor  --get 	
+	//((retornando o valor(string) colocando atributo(numero)) -set
 
 	public int getPosX() {
 		return posX;
