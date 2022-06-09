51d9fe3d7f5bf35917d2eb52997913a96de3eae9
isabelly <102962658+Isabelly3@users.noreply.github.com>
2022-06-09 02:24:14 -0300

    Update Aplicacao.java

--- ClassesAbstratas/src/Aplicacao.java
+++ ClassesAbstratas/src/Aplicacao.java
@@ -5,5 +5,16 @@ public class Aplicacao {
 		// TODO Auto-generated method stub
 
 	}
+	Item item = new Cogumelo (); //Instância o objeto cogumelo e guarda em item (variavel)
+		item.pegar();
+		
+		item = new Moeda();//Instância o objeto moeda e guarda em item (variavel)
+		item.pegar();
+		
+		item = new Diamante(); //Instância o objeto diamante e guarda em item (variavel)
+		item.pegar();
+	}
 
 }
+
+
